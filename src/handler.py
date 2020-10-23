bccc3266662323b9928d06db52ed21ca8952687e
ManuelDiazPozo <manu_dp_3@hotmail.com>
2020-10-23 14:30:40 +0200

    Solucionado problema de la salida del container: la última columna contiene un retorno de carro en el nombre.

--- src/handler.py
+++ src/handler.py
@@ -5,19 +5,18 @@ import pandas as pd
 path = os.getenv("Http_Path")
 method = os.getenv("Http_Method")
 
-def process_post(req):
+def process_post(req):   
     flowfile = [line.split(',') for line in req.split('\n')]
     flowfile = pd.DataFrame(data=flowfile[1:], columns=flowfile[0])
-    flowfile.drop(flowfile.tail(1).index, inplace=True)
     flowfile['RSE'].replace('', 0, inplace=True)
-    averages = dict.fromkeys(set(flowfile['MsCode'].values))
+    averages = dict.fromkeys(flowfile['MsCode'].values)
     for key,_ in averages.items():
         averages[key] = flowfile['RSE'].where(flowfile['MsCode'] == key).astype(float).mean()
-
+    
     flowfile['RSE'].replace(0, '', inplace=True)
     flowfile['Avg_RSE'] = flowfile['MsCode'].replace(averages)
-
-    return flowfile.to_csv(index=False)
+    
+    return flowfile[['MsCode','Estimate','RSE','Avg_RSE','Flag\r']].to_csv(index=False)
 
 def handle(req):
     """handle a request to the function
